6648f54d7ac150961858102f23e3399ae16255c5
patriciax <yively.pa@gmail.com>
2020-04-29 09:36:43 +0200

    voews

--- assets/js/setting-slick.js
+++ assets/js/setting-slick.js
@@ -101,3 +101,37 @@ $('.main-testimonios__content').slick({
     }
   ]
 });
+
+$('.main-marcas__content').slick({
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 2,
+  dots: false,
+  arrows: false,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+   
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
